3e0fabf914ed6d2103b9eb542746f70ea6cf8c90
LuytBui <luytbq43@gmail.com>
2022-01-19 09:57:04 +0700

    done

---
+++ src/ProductPriceComparator.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+
+public class ProductPriceComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product product1, Product product2) {
+        if (product1.getPrice() - product2.getPrice() > 0) {
+            return 1;
+        } else if (product1.getPrice() - product2.getPrice() < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
